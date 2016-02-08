d246de9fa523d930b833aef1864506c4c2c36f8c
Vignesh Gurusamy <vignesh@linkstreet.in>
2016-02-08 14:42:12 +0530

    Unit test & included phpunit.xml file
    Using orchestra/testbench and phpunit/phpunit

---
+++ tests/Adapters/KapAdapterTest.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Linkstreet\LaravelSms\Tests\Adapters;
+
+use Linkstreet\LaravelSms\Tests\TestCase;
+use Sms;
+
+class KapAdapterTest extends TestCase
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('sms.adapter.kap', [
+            'username' => rand(),
+            'password' => rand(),
+            'sender' => 'FEDCBA'
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->manager = Sms::app(Sms::KapAdapter());
+    }
+
+    /**
+     * @test
+     * @expectedException Linkstreet\LaravelSms\Exceptions\AdapterException
+     */
+    public function emptyUsernameConfig()
+    {
+        Sms::kapAdapter(['username' => '']);
+    }
+
+    /**
+     * @test
+     * @expectedException Linkstreet\LaravelSms\Exceptions\AdapterException
+     */
+    public function emptyPasswordConfig()
+    {
+        Sms::kapAdapter(['username' => 'test', 'password' => '']);
+    }
+
+    /**
+     * @test
+     * @expectedException Linkstreet\LaravelSms\Exceptions\AdapterException
+     */
+    public function emptySenderConfig()
+    {
+        Sms::kapAdapter(['username' => 'test', 'password' => 'test', 'sender' => '']);
+    }
+
+    /**
+     * @test
+     */
+    public function sendSms()
+    {
+        $stub = Sms::kapAdapter();
+
+        // Create a mock and queue two responses.
+        $mock = new \GuzzleHttp\Handler\MockHandler([
+            new \GuzzleHttp\Psr7\Response(
+                200,
+                ['Content-type' => 'application/json'],
+                json_encode(
+                    [
+                        'results' => [
+                            [
+                                'status' => 0,
+                                'message_id' => '12312314122',
+                                'destination' => '+910000000001'
+                            ],
+                            [
+                                'status' => -13,
+                                'message_id' => '',
+                                'destination' => '00000'
+                            ]
+                        ]
+                    ]
+                )
+            ),
+        ]);
+
+        // Configure the stub.
+        $stub->setClient(new \GuzzleHttp\Client(['handler' => \GuzzleHttp\HandlerStack::create($mock)]));
+
+        $response = Sms::app($stub)
+            ->to($this->devices)
+            ->send($this->message);
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertTrue((bool) $response->getSuccessCount());
+        $this->assertTrue((bool) $response->getFailureCount());
+        $this->assertCount(1, $response->getSuccessRecipient());
+        $this->assertCount(1, $response->getFailureRecipient());
+        $this->assertObjectHasAttribute('results', json_decode($response->getRaw()->getBody()));
+        $this->assertEquals(2, $response->getDeviceCount());
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Linkstreet\LaravelSms\Tests;
+
+use Sms;
+
+abstract class TestCase extends \Orchestra\Testbench\TestCase
+{
+    public $device;
+
+    public $devices;
+
+    public $message;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getPackageProviders($app)
+    {
+        return ['Linkstreet\LaravelSms\Providers\SmsServiceProvider'];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getPackageAliases($app)
+    {
+        return [
+            'Sms' => 'Linkstreet\LaravelSms\Facades\Sms'
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->device = Sms::Device('+910000000000');
+        $this->testDevice();
+        $this->devices = Sms::DeviceCollection([$this->device, Sms::Device('00000')]);
+        $this->testDeviceCollection();
+        $this->message = Sms::Message('Hello world');
+        $this->testMessage();
+    }
+
+    /**
+     * Test the device model
+     */
+    public function testDevice()
+    {
+        $this->assertSame('+910000000001', $this->device->setNumber('+910000000001')->getNumber());
+    }
+
+    /**
+     * Test the device collcetion & device
+     */
+    public function testDeviceCollection()
+    {
+        $count = 0;
+        foreach ($this->devices as $device) {
+            $count++;
+        }
+        $this->assertSame($count, $this->devices->count());
+        $this->assertSame($this->device, $this->devices->get('+910000000001'));
+    }
+
+    /**
+     * Test the message model
+     */
+    public function testMessage()
+    {
+        $this->assertSame('Hello world!', $this->message->setMessage('Hello world!')->getMessage());
+    }
+}
